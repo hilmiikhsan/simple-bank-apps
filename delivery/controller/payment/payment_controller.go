1fc8c8ef97daba9209d069ffcf7528ab5aae3953
M IKHSAN HILMI <ikhsanhilmimuhammad@gmail.com>
2023-10-25 20:50:16 +0700

    feat: add payment controller

---
+++ delivery/controller/payment/payment_controller.go
@@ -0,0 +1,54 @@
+package payment
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/simple-bank-apps/dto"
+	"github.com/simple-bank-apps/middleware"
+	"github.com/simple-bank-apps/usecase/payment"
+	"github.com/simple-bank-apps/utils"
+	"github.com/simple-bank-apps/validation"
+	"github.com/sirupsen/logrus"
+)
+
+type PaymentController struct {
+	router         *gin.Engine
+	paymentUsecase payment.PaymentUsecase
+}
+
+func (p *PaymentController) Payment(c *gin.Context) {
+	var req dto.PaymentRequest
+	userID := middleware.GetTokenMiddlewareFromContext(c.Request.Context()).ID
+
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, utils.NewBadRequest(validation.FieldErrors(err)))
+		return
+	}
+
+	err := p.paymentUsecase.Payment(c.Request.Context(), req, userID)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, utils.NewInternalServerError(err.Error()))
+		return
+	}
+
+	c.JSON(http.StatusOK, utils.NewSuccessResponse(http.StatusOK, "Success Payment"))
+}
+
+func NewPaymentController(
+	router *gin.Engine,
+	paymentUsecase payment.PaymentUsecase,
+	tokenMiddleware middleware.TokenMiddleware,
+	logger middleware.LogMiddleware,
+) {
+	controller := &PaymentController{
+		router:         router,
+		paymentUsecase: paymentUsecase,
+	}
+
+	routerGroup := router.Group("/api/v1/payment")
+	routerGroup.Use(tokenMiddleware.TokenMiddlewareAuthorize())
+	routerGroup.Use(logger.LogRequestMiddleware(logrus.New()))
+
+	routerGroup.POST("/", controller.Payment)
+}
